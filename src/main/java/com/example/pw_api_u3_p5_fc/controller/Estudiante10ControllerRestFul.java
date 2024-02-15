c88aaa350c1d1faac044c36d3173b2b87b5d7b32
AlexCondor <alexguerron@gmail.com>
2024-02-15 11:21:42 -0500

    add @CrossOrigin

--- src/main/java/com/example/pw_api_u3_p5_fc/controller/Estudiante10ControllerRestFul.java
+++ src/main/java/com/example/pw_api_u3_p5_fc/controller/Estudiante10ControllerRestFul.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,7 +32,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 
-
+@CrossOrigin
 @RestController
 @RequestMapping(path="/estudiantes10")
 public class Estudiante10ControllerRestFul {
